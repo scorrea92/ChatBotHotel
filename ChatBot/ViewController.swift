fd9756f9d2128ebed82ee7d9987c92093ea0d919
Sebastian Correa <scorrea92@hotmail.com>
2018-01-29 10:23:02 +0100

    Trabajo final reconocimiento automatico del habla, chat bot para reserva de habitaciones de hoteles.

---
+++ ChatBot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChatBot
+//
+//  Created by Sebastian Correa on 23/1/18.
+//  Copyright © 2018 Sebastian Correa. All rights reserved.
+//
+
+import UIKit
+import ApiAI
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfInput: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
